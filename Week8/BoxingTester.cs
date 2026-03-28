ec96ce498570560f056cc2c94edc19752493e1c0
User <aksenkina.irra1@gmail.com>
2026-03-28 17:46:44 +0200

    перший етап

---
+++ Week8/BoxingTester.cs
@@ -0,0 +1,14 @@
+namespace Week8;
+
+public static class BoxingTester
+{
+    public static void Test()
+    {
+        int a = 10;
+// TODO:
+// 1. box a into object
+// 2. unbox back into int
+// 3. print all values
+// 4. add comments explaining what happens
+    }
+}
--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,18 @@
-﻿
-class Program
-{
-    public static void Main()
-    {
-        
-    }
-    
-}
+﻿using Week8;
+
+
+// Value type demo
+Point p1 = new Point { X = 1, Y = 2 };
+Point p2 = p1;
+
+
+// TODO: modify p2 and print both p1 and p2
+Console.WriteLine();
+
+// Reference type demo
+PointRef r1 = new PointRef { X = 1, Y = 2 };
+PointRef r2 = r1;
+// TODO: modify r2 and print both r1 and r2
+Console.WriteLine();
+// Boxing demo
+BoxingTester.Test();
