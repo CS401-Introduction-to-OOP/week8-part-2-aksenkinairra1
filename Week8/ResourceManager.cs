03fb3fd3000c9cbddfadd7665aa3b1de33d7a15b
User <aksenkina.irra1@gmail.com>
2026-03-28 17:28:29 +0200

    другий етап

--- Week8/ResourceManager.cs
+++ Week8/ResourceManager.cs
@@ -6,17 +6,20 @@ public class ResourceManager<T> where T : Resource
     private readonly List<T> _resources = new();
     public void Add(T resource)
     {
-// TODO: add resource to list
-        throw new NotImplementedException();
+        _resources.Add(resource);
     }
     public void OpenAll()
     {
-// TODO: open every resource
-        throw new NotImplementedException();
+        foreach (var resource in _resources)
+        {
+            resource.Open();
+        }
     }
     public void CloseAll()
     {
-// TODO: close every resource
-        throw new NotImplementedException();
+        foreach (var resource in _resources)
+        {
+            resource.Close();
+        }
     }
 }
