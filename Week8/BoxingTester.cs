5c494f3f89f9fb9de55fd3916daac356a2253c50
User <aksenkina.irra1@gmail.com>
2026-03-28 17:51:57 +0200

    другий етап

--- Week8/BoxingTester.cs
+++ Week8/BoxingTester.cs
@@ -5,10 +5,15 @@ public static class BoxingTester
     public static void Test()
     {
         int a = 10;
-// TODO:
-// 1. box a into object
-// 2. unbox back into int
-// 3. print all values
-// 4. add comments explaining what happens
+        object b = a;
+        int c = (int)b;
+        
+        Console.WriteLine($"Original int a: {a}");
+        Console.WriteLine($"Boxed object b: {b}");
+        Console.WriteLine($"Unboxed int c: {c}");
+        
+        // Boxing: копіюємо значення 'a' у новий об'єкт у купі
+        // Unboxing: копіюємо значення з об'єкта в купі назад у стек
+        // Print
     }
 }
