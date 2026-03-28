c9b586debf5497cbb9c4a49c407503e6f33fd208
User <aksenkina.irra1@gmail.com>
2026-03-28 17:32:57 +0200

    третій етап

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -3,9 +3,15 @@
 var file = new FileResource("report.txt");
 var network = new NetworkResource("api.company.local");
 var manager = new ResourceManager<Resource>();
-// TODO:
-// 1. Add resources to manager
-// 2. Open all resources
-// 3. Demonstrate using with one disposable resource
-// 4. Close all resources
+
+manager.Add(file);
+manager.Add(network);
+manager.OpenAll();
+
+using (var singleRes = new FileResource("temp.log"))
+{
+    singleRes.Open();
+}
+
+manager.CloseAll();
 Console.WriteLine("Done.");
